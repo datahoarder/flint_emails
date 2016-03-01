ab590cc31d7ed18761058793a33bc2c038770cf0
Dan Nguyen <dansonguyen@gmail.com>
2016-02-29 22:04:30 -0800

    first

---
+++ fetch.py
@@ -0,0 +1,19 @@
+from os import makedirs
+from os.path import basename, join, exists
+import yaml
+import requests
+DOCS_LIST = 'docs.yaml'
+DOCS_DIR = 'docs'
+
+with open(DOCS_LIST) as df:
+    for dateslug, docsdict in yaml.load(df).items():
+        for url in docsdict['urls']:
+            dirname = join(DOCS_DIR, dateslug)
+            makedirs(dirname, exist_ok=True)
+            fname = join(dirname, basename(url))
+            if not exists(fname):
+                print("Downloading", url)
+                resp = requests.get(url)
+                with open(fname, 'wb') as wf:
+                    print("\tSaving to", fname)
+                    wf.write(resp.content)
